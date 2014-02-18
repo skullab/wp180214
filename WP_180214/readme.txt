334b3c5165db5216b859e83a82a6bb8f6fe388f5
ivan maruca <ivan.maruca@gmail.com>
2014-02-18 02:07:59 +0100

    Release ver 0.0.1 [structure]

---
+++ WP_180214/readme.txt
@@ -0,0 +1,7 @@
+=== WP180214 ===
+Contributors: ivanmaruca
+Requires at least: 3.7.1
+Tested up to: 3.8.1
+Stable tag: 0.0.1
+License: Apache License, Version 2.0
+License URI: http://www.apache.org/licenses/LICENSE-2.0.html
---
+++ WP_180214/wp180214.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: WP180214
+Plugin URI: http://www.skullab.com
+Description: Questo plugin &egrave; stato sviluppato ad uso esclusivo del richiedente e non &egrave; disponibile in nessun repository pubblico.Per ulteriori dettagli contattare il fornitore dei servizi e/o il programmatore del plugin. PROCEDURA DI INSTALAZZIONE : (1) Cliccare su "Attiva" a sinistra di questa descrizione (2) Accedere alla pagina delle Impostazioni per apportare modifiche al plugin.Per ulteriori dettagli si consiglia la lettura della <strong>documentazione allegata</strong>.
+Version: 0.0.1
+Author: <A HREF="mailto:ivan.maruca@gmail.com?subject=WPplugin_vetrinaImmobiliare">Ivan Maruca</A>
+Author URI: http://www.skullab.com
+License: Apache License, Version 2.0
+*/
+
+/*
+Copyright 2014 Ivan Maruca
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+?>
